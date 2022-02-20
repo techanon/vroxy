6c33a95617b1e7c52cdfc65fb896c29c540ae916
techanon <dev@techanon.dev>
2022-02-20 14:37:53 -0600

    Update to use async web server. Add caching. Add pooling for ytdl executions. Update readme to note that yt-dlp should be used as ytdl does not support format sorting, only filtering.

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # qroxy
-Quest VR Headset Proxy for VRChat.
+Self-hosted light-weight server proxy for YoutubeDL.
 
-Run with python3 and yt-dlp installed.
+Run with python3 and yt-dlp (or your preferred fork of youtubeDL) installed.
 
 Currently in alpha pending next release with early caching.
+
+Prep:
+- Setup a public facing server (optionally with an associated domain name)
+    It is _highly_ recommended to use a reverse proxy like NGINX to handle HTTPS frontloading of the server's public facing entry point and having that proxy the call to the _actual_ localhost endpoint
+- Install python3 with pip
+- Get the latest copy of [ytdlp](https://github.com/yt-dlp/yt-dlp/releases/latest)
+- Clone this repo to the server at your desired file path
+- `cd` into the that file path in your desired command line
+- Install the dependant packages for python via `pip install yt_dlp aiohttp`
+- Rename example.ini to settings.ini and change the settings as you need
+
+Usage:
+Run the server via `python3 /path/to/repo/qroxy.py`
+Then access the url via `https://mydomainorip/?url=https://youtube.com/watch?v=VIDEO_ID` to receive a 301 redirect to the direct link video url.
--- qroxy.py
+++ qroxy.py
@@ -1,49 +1,143 @@
 from __future__ import unicode_literals
-import yt_dlp
-from http.server import BaseHTTPRequestHandler, HTTPServer
+from configparser import ConfigParser
+from asyncio import sleep
 import time
-# Need these to get low level url stuff.
-from urllib.parse import urlparse, parse_qs
+import re
+from os import path
+from urllib.parse import urlparse
+
+from aiohttp import web
+from yt_dlp import YoutubeDL
+
+from normalize import normalizeUrl
 
 
-ydl_opts = {
-    'format': 'best',
-    'simulate': True,
-    'forceurl': True
-    }
+class PoolCount:
+    def __init__(self):
+        self.count = 0
 
-hostName = "localhost"
-serverPort = 8080
+    def add(self):
+        self.count += 1
 
-class MyServer(BaseHTTPRequestHandler):
-    def do_GET(self):
-        print("test")
-        query_components = parse_qs(urlparse(self.path).query)
-        url = query_components["url"]
-        print(url[0])
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            result = ydl.extract_info("{}".format(url[0]))
-        self.send_response(301)
-        self.send_header('Location',result['url'])
-        self.end_headers()
+    def remove(self):
+        self.count -= 1
 
-    def do_HEAD(self):
-        query_components = parse_qs(urlparse(self.path).query)
-        url = query_components["url"]
-        print(url[0])
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            result = ydl.extract_info("{}".format(url[0]))
-        self.send_response(301)
-        self.send_header('Location',result['url'])
-        self.end_headers()
-if __name__ == "__main__":        
-    webServer = HTTPServer((hostName, serverPort), MyServer)
-    print("Server started http://%s:%s" % (hostName, serverPort))
 
-    try:
-        webServer.serve_forever()
-    except KeyboardInterrupt:
-        pass
+class Item:
+    def __init__(self, url, mode):
+        self.original_url = url
+        self.hostname = urlparse(url).hostname
+        self.resolved_url = None
+        self.resolved_id = None
+        self.mode = mode
+        self.expiry = 0
+        self.processing = True
 
-    webServer.server_close()
-    print("Server stopped.")
+    def resolve(self, f):
+        self.resolved_url = f['url']
+        self.resolved_id = f['format_id']
+        self.expiry = self.extractExpiry()
+        self.processing = False
+
+    def extractExpiry(self):
+        p = expire_regex.search(self.resolved_url)
+        if (p is not None):
+            return int(p.group(1))
+        return time.time() + 600 # default to 10 minutes
+    
+
+
+expire_regex = re.compile(r"exp(?:ir(?:es?|ation))?=(\d+)")
+def format_selector(format_list, item):
+    format_list.reverse()
+
+    # explicitly filter for a specific format type when looking for HQ vimeo links
+    if item.mode == 1 and item.hostname in ["vimeo.com"]:
+        t_list = [
+            f
+            for f in format_list
+            if str.startswith("hls_akafire-interconnect_quic", f["format_id"])
+        ]
+        if len(t_list) > 0:
+            format_list = t_list
+
+    best = format_list[0]
+    print(best["url"])
+    return best
+
+
+config = ConfigParser({"server": {"host": "localhost", "port": "8008"}})
+if path.isfile("./settings.ini"):
+    config.read("settings.ini")
+
+mode_map = {"default": 0, "0": 0, "high": 1, "1": 1, "low": 2, "2": 2}
+cache_map = {}
+sort_opts = {
+    0: ["hasvid", "hasaud", "res:1080"],
+    1: ["hasvid", "hasaud", "res:9001"],
+    2: ["hasvid", "hasaud", "res:480"],
+}
+
+pool_max = 10
+pool = PoolCount()
+
+
+routes = web.RouteTableDef()
+
+@routes.view("/")
+class YTDLProxy(web.View):
+    async def head(self):
+        if not self.request.query.get("url"):
+            return web.Response(status=404)
+        return web.Response(status=302, headers={"Location": await self.resolveUrl()})
+
+    async def get(self):
+        if not self.request.query.get("url"):
+            return web.Response(status=404, text="Missing Url Param")
+        return web.Response(status=302, headers={"Location": await self.resolveUrl()})
+
+    async def resolveUrl(self):
+        mode = self.request.query.get("m") or "default"
+        mode = mode_map[mode]
+        url = normalizeUrl(self.request.query.get("url"))
+        _id = f"{mode}~{url}"
+        if _id in cache_map:
+            item = cache_map[_id]
+            if item.expiry < time.time():
+                print("Cache expired")
+                del cache_map[_id]
+            else:
+                # wait until the other request for the same url resolves, 
+                # then use the cached url from that
+                while item.processing:
+                    await sleep(1)
+                print(f"Resolving mode {mode} for url: {url}")
+                print("Cache hit")
+                print(item.resolved_url)
+                print(f"{item.resolved_id} expires in {item.expiry - time.time()} seconds")
+                return item.resolved_url or ""
+
+        cache_map[_id] = item = Item(url, mode)
+
+        # wait for an pool slot to open
+        while pool.count >= pool_max:
+            await sleep(1)
+
+        ytdl_opts = {"quiet": True, "format_sort": sort_opts[mode]}
+        with YoutubeDL(ytdl_opts) as ytdl:
+            pool.add()
+            print(f"Resolving mode {mode} for url: {url}")
+            print("Fetching fresh info")
+            result = ytdl.extract_info(url, download=False)
+            format_item = format_selector(result["formats"], item)
+            pool.remove()
+            item.resolve(format_item)
+            print(f"{item.resolved_id} expires in {item.expiry - time.time()} seconds")
+
+        print("")
+        return item.resolved_url or ""
+
+
+app = web.Application()
+app.add_routes(routes)
+web.run_app(app, host=config["server"]["host"], port=config["server"]["port"])
