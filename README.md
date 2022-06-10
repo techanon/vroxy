93d2e5a76bf9e5279e513495320e59568675266b
techanon <dev@techanon.dev>
2022-06-09 21:45:58 -0500

    Fix and improve logging information. Add f and s optional parameters for format id and custom sort. Add additional sorting presets for different uses. Add request timeout check for max of 30 seconds waiting for a URL to be resolved. Update 302 redirect to 307 redirect. Add vimeo specific sort preference for m3u8 links.

--- README.md
+++ README.md
@@ -8,13 +8,23 @@ Currently in alpha pending next release with early caching.
 Prep:
 - Setup a public facing server (optionally with an associated domain name)
     It is _highly_ recommended to use a reverse proxy like NGINX to handle HTTPS frontloading of the server's public facing entry point and having that proxy the call to the _actual_ localhost endpoint
-- Install python3 with pip
+- Install python3 (make sure the `pip` tool is also installed)
 - Get the latest copy of [ytdlp](https://github.com/yt-dlp/yt-dlp/releases/latest)
 - Clone this repo to the server at your desired file path
 - `cd` into the that file path in your desired command line
-- Install the dependant packages for python via `pip install yt_dlp aiohttp`
+- Install the dependant packages for python via `python3 -m pip install -U yt-dlp aiohttp`
 - Rename example.ini to settings.ini and change the settings as you need
 
 Usage:
 Run the server via `python3 /path/to/repo/qroxy.py`
 Then access the url via `https://mydomainorip/?url=https://youtube.com/watch?v=VIDEO_ID` to receive a 301 redirect to the direct link video url.
+
+Optional parameters:
+- `f`: Specific format id for the given url that is desired. This is something that is looked up ahead of time via manually checking --list-formats in ytdl.
+- `s`: Custom sort order for ytdl to determine what it thinks is 'best'. Sort order options can be found [Here](https://github.com/yt-dlp/yt-dlp/blob/release/README.md#sorting-formats)
+- `m`: Specific presets for sort order options
+    - `0` - aka "hasvid,hasaud,res:1440" for decent sized media with audio+video, generally compatible with all platforms
+    - `1` - aka "hasvid,hasaud,res" for sort preferring audio+video with the highest quality
+    - `2` - aka "hasvid,hasaud,+res" for sort preferring audio+video with the lowest quality
+    - `3` - aka "hasvid,res,codec:vp9" for sort preferring highest quality with priority on VP9 codec for platform compatibility
+    - `4` - aka "hasvid,res" for sort preferring highest quality without concern for codec or audio
--- qroxy.py
+++ qroxy.py
@@ -24,42 +24,35 @@ class PoolCount:
 
 
 class Item:
-    def __init__(self, url, mode):
+    def __init__(self, url, sort):
         self.original_url = url
         self.hostname = urlparse(url).hostname
         self.resolved_url = None
         self.resolved_id = None
-        self.mode = mode
+        self.sort = sort
         self.expiry = 0
         self.processing = True
 
     def resolve(self, f):
-        self.resolved_url = f['url']
-        self.resolved_id = f['format_id']
+        self.resolved_url = f["url"]
+        self.resolved_id = f["format_id"]
         self.expiry = self.extractExpiry()
         self.processing = False
 
     def extractExpiry(self):
         p = expire_regex.search(self.resolved_url)
-        if (p is not None):
-            return int(p.group(1))
-        return time.time() + 600 # default to 10 minutes
-    
+        if p is not None: return int(p.group(1))
+        return time.time() + 600  # default to 10 minutes
 
 
 expire_regex = re.compile(r"exp(?:ir(?:es?|ation))?=(\d+)")
+
+
 def format_selector(format_list, item):
     format_list.reverse()
-
-    # explicitly filter for a specific format type when looking for HQ vimeo links
-    if item.mode == 1 and item.hostname in ["vimeo.com"]:
-        t_list = [
-            f
-            for f in format_list
-            if str.startswith("hls_akafire-interconnect_quic", f["format_id"])
-        ]
-        if len(t_list) > 0:
-            format_list = t_list
+    # print(format_list[0].keys())
+    # for form in format_list: print(form["format_id"])
+    # print("", flush=True)
 
     best = format_list[0]
     print(best["url"])
@@ -70,12 +63,30 @@ config = ConfigParser({"server": {"host": "localhost", "port": "8008"}})
 if path.isfile("./settings.ini"):
     config.read("settings.ini")
 
-mode_map = {"default": 0, "0": 0, "high": 1, "1": 1, "low": 2, "2": 2}
+mode_map = {
+    # default
+    "0": 0,
+    # avhigh
+    "1": 1,
+    # avlow
+    "2": 2,
+    # hqvidcompat
+    "3": 3,
+    # hqvidbest
+    "4": 4,
+}
 cache_map = {}
 sort_opts = {
-    0: ["hasvid", "hasaud", "res:1080"],
-    1: ["hasvid", "hasaud", "res:9001"],
-    2: ["hasvid", "hasaud", "res:480"],
+    # decent sized media with audio+video, generally compatible with all platforms
+    0: ["hasvid", "hasaud", "res:1440"],
+    # sort preferring audio+video with the highest quality
+    1: ["hasvid", "hasaud", "res"],
+    # sort preferring audio+video with the lowest quality
+    2: ["hasvid", "hasaud", "+res"],
+    # sort preferring highest quality with priority on VP9 codec for platform compatibility
+    3: ["hasvid", "res", "codec:vp9"],
+    # sort preferring highest quality without concern for codec or audio
+    4: ["hasvid", "res"],
 }
 
 pool_max = 10
@@ -84,55 +95,86 @@ pool = PoolCount()
 
 routes = web.RouteTableDef()
 
+
 @routes.view("/")
 class YTDLProxy(web.View):
     async def head(self):
         if not self.request.query.get("url"):
             return web.Response(status=404)
-        return web.Response(status=302, headers={"Location": await self.resolveUrl()})
+        return await self.process()
 
     async def get(self):
         if not self.request.query.get("url"):
             return web.Response(status=404, text="Missing Url Param")
-        return web.Response(status=302, headers={"Location": await self.resolveUrl()})
+        return await self.process()
+
+    async def process(self):
+        url = await self.resolveUrl()
+        if url: return web.Response(status=307, headers={"Location": url})
+        else: return web.Response(status=408)
 
     async def resolveUrl(self):
-        mode = self.request.query.get("m") or "default"
-        mode = mode_map[mode]
+
+        # silence the output of ytdl
+        ytdl_opts = {"quiet": True}
+
         url = normalizeUrl(self.request.query.get("url"))
-        _id = f"{mode}~{url}"
+        mode = mode_map[self.request.query.get("m") or "0"]
+        fid = self.request.query.get("f")
+        host = urlparse(url).hostname
+        # if format ID is provided, retrieve that explicitly
+        if fid:
+            ytdl_opts["f"] = fid
+            cacheId = fid
+        # otherwise use the "best" sorting based on the sort available
+        else:
+            # use either the given user sort, or extrapolate for the given preset mode
+            s = self.request.query.get("s")
+            if (s): sort = s.replace(" ", "").split(",")
+            else: sort = list(sort_opts[mode])
+            try: 
+                if host.index("vimeo") > -1:
+                    sort.append("proto:m3u8_native")
+            except: pass
+            ytdl_opts["format_sort"] = sort
+            cacheId = ",".join(sort)
+
+        _id = f"{cacheId}~{url}"
         if _id in cache_map:
             item = cache_map[_id]
             if item.expiry < time.time():
                 print("Cache expired")
                 del cache_map[_id]
             else:
-                # wait until the other request for the same url resolves, 
+                # wait until the other request for the same url resolves,
                 # then use the cached url from that
                 while item.processing:
                     await sleep(1)
-                print(f"Resolving mode {mode} for url: {url}")
+                print(f"Resolving '{cacheId}' for url: {url}")
                 print("Cache hit")
                 print(item.resolved_url)
-                print(f"{item.resolved_id} expires in {item.expiry - time.time()} seconds")
+                print(
+                    f"{item.resolved_id} expires in {item.expiry - time.time()} seconds", flush=True
+                )
                 return item.resolved_url or ""
 
-        cache_map[_id] = item = Item(url, mode)
+        cache_map[_id] = item = Item(url, sort)
 
         # wait for an pool slot to open
+        timeout = time.time() + 30  # 30 seconds timelimit for waiting
         while pool.count >= pool_max:
+            if time.time() > timeout: return None
             await sleep(1)
 
-        ytdl_opts = {"quiet": True, "format_sort": sort_opts[mode]}
         with YoutubeDL(ytdl_opts) as ytdl:
             pool.add()
-            print(f"Resolving mode {mode} for url: {url}")
-            print("Fetching fresh info")
+            print(f"Resolving '{cacheId}' for url: {url}")
+            print("Fetching fresh info", flush=True)
             result = ytdl.extract_info(url, download=False)
             format_item = format_selector(result["formats"], item)
             pool.remove()
             item.resolve(format_item)
-            print(f"{item.resolved_id} expires in {item.expiry - time.time()} seconds")
+            print(f"{item.resolved_id} expires in {item.expiry - time.time()} seconds", flush=True)
 
         print("")
         return item.resolved_url or ""
@@ -140,4 +182,5 @@ class YTDLProxy(web.View):
 
 app = web.Application()
 app.add_routes(routes)
+print("Starting Qroxy server.")
 web.run_app(app, host=config["server"]["host"], port=config["server"]["port"])
