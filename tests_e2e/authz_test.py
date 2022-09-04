41a9b89b141a8768661e403bab761f45c2957f83
frizzle-chan <frizzle@frizzle.wtf>
2022-09-04 19:47:55 +0000

    fmt

--- tests_e2e/authz_test.py
+++ tests_e2e/authz_test.py
@@ -1,30 +1,38 @@
 import re
 import requests
 
+
 def test_authz_token():
     msg = "Missing authorization token"
-    r = requests.get('http://vroxy_e2e_authz:8008/healthz')
+    r = requests.get("http://vroxy_e2e_authz:8008/healthz")
     assert r.status_code == 401
     assert r.text == msg
-    r = requests.get('http://vroxy_e2e_authz:8008/healthz?token=abc')
+    r = requests.get("http://vroxy_e2e_authz:8008/healthz?token=abc")
     assert r.status_code == 200
     assert r.text == "OK"
-    r = requests.get('http://vroxy_e2e_authz:8008/healthz?token=123')
+    r = requests.get("http://vroxy_e2e_authz:8008/healthz?token=123")
     assert r.status_code == 200
     assert r.text == "OK"
-    r = requests.get('http://vroxy_e2e_authz:8008/healthz?token=bogus')
+    r = requests.get("http://vroxy_e2e_authz:8008/healthz?token=bogus")
     assert r.status_code == 401
     assert r.text == msg
-    r = requests.get('http://vroxy_e2e_authz:8008/healthz', headers={"Authorization": "Bearer abc"})
+    r = requests.get(
+        "http://vroxy_e2e_authz:8008/healthz", headers={"Authorization": "Bearer abc"}
+    )
     assert r.status_code == 200
     assert r.text == "OK"
-    r = requests.get('http://vroxy_e2e_authz:8008/healthz', headers={"Authorization": "Bearer 123"})
+    r = requests.get(
+        "http://vroxy_e2e_authz:8008/healthz", headers={"Authorization": "Bearer 123"}
+    )
     assert r.status_code == 200
     assert r.text == "OK"
-    r = requests.get('http://vroxy_e2e_authz:8008/healthz', headers={"Authorization": "Bearer bogus"})
+    r = requests.get(
+        "http://vroxy_e2e_authz:8008/healthz", headers={"Authorization": "Bearer bogus"}
+    )
     assert r.status_code == 401
     assert r.text == msg
-    r = requests.get('http://vroxy_e2e_authz:8008/healthz', headers={"Authorization": "bogus"})
+    r = requests.get(
+        "http://vroxy_e2e_authz:8008/healthz", headers={"Authorization": "bogus"}
+    )
     assert r.status_code == 401
     assert r.text == msg
-
