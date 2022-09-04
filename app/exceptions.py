55b8d26cd6c9ab846d37c21d16eb5082dc63ce66
techanon <dev@techanon.dev>
2022-09-04 05:25:45 -0500

    Condense exceptions file formatting

--- app/exceptions.py
+++ app/exceptions.py
@@ -1,20 +1,7 @@
-class Error400BadRequest(Exception):
-    pass
-
-class Error403Forbidden(Exception):
-    pass
-
-class Error403Whitelist(Exception):
-    pass
-
-class Error404NotFound(Exception):
-    pass
-
-class Error408Timeout(Exception):
-    pass
-
-class Error410Gone(Exception):
-    pass
-
-class Error429TooManyRequests(Exception):
-    pass
+class Error400BadRequest(Exception): pass
+class Error403Forbidden(Exception): pass
+class Error403Whitelist(Exception): pass
+class Error404NotFound(Exception): pass
+class Error408Timeout(Exception): pass
+class Error410Gone(Exception): pass
+class Error429TooManyRequests(Exception): pass
