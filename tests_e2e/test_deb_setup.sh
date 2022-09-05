096560ad4caad97703802f22b1dc0ac823ad4769
frizzle-chan <86853733+frizzle-chan@users.noreply.github.com>
2022-09-05 17:57:56 -0500

    Apply suggestions from code review

--- tests_e2e/test_deb_setup.sh
+++ tests_e2e/test_deb_setup.sh
@@ -8,7 +8,7 @@ cleanup() {
 
 cleanup
 
-# trap cleanup EXIT
+trap cleanup EXIT
 
 docker compose up -d --wait pebble
 docker compose up -d vroxy-install-e2e.test
@@ -18,11 +18,9 @@ DRUN="docker compose exec vroxy-install-e2e.test"
 $DRUN ./tests_e2e/test_deb_bootstrap.sh
 $DRUN ./vroxy_install_deb.sh
 sleep 15
-# test tmux session
 echo "## Testing tmux session"
 $DRUN curl -f http://localhost:8420/healthz
 echo
-# test nginx
 echo "## Testing nginx"
 $DRUN curl -f -H 'Host: vroxy-install-e2e.test' -k https://localhost/healthz
 echo
